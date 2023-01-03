b8f6cb7ed3130ae4ab62b509786b9f85913be262
Daan Schmitz <d.schmitz@amsterdam.nl>
2023-01-03 16:16:05 +0100

    Refactor download.py

--- python/download.py
+++ python/download.py
@@ -3,10 +3,8 @@ import zipfile
 import pathlib
 import shutil
 
-DEST_FOLDER = "C:/python_projects/_uitproberen/test_project/src/"
 
-
-def download(url: str, dest_path):
+def download(url: str, dest_path: pathlib.Path):
     r = requests.get(url, stream=True)
     if not r.ok:
         raise ValueError("Download failed: check repo and language")
@@ -17,36 +15,29 @@ def download(url: str, dest_path):
                 f.write(chunk)
 
 
-def unzip(zip_store, dest_folder):
+def unzip(zip_store, dest_folder: pathlib.Path):
     with zipfile.ZipFile(zip_store) as zipfile_:
         for filename in zipfile_.namelist():
             zipfile_.extract(filename, path=dest_folder)
-
-
-def move(dest_folder, ori_folder) -> None:
-    # p_ori = dest_folder / "tools-onderzoek-en-statistiek-main" / language
-    # p_dest = dest_folder / "os_tools"
-    # p_del = dest_folder / "tools-onderzoek-en-statistiek-main"
-
-    print(f"{dest_folder=}")
-    print(f"{ori_folder=}")
-
-    if dest_folder.exists():
-        shutil.rmtree(dest_folder)
-
-    shutil.move(ori_folder, dest_folder)
-    # shutil.rmtree(p_del)
 
 
 class FileLocation:
     git_url = "https://gitlab.com/os-amsterdam/"
     language = "python"
 
-    def __init__(self, dest_folder, repo, branch, language):
+    def __init__(
+        self,
+        dest_folder: str,
+        dest_folder_name: str,
+        repo: str,
+        branch: str,
+        subfolder: str,
+    ):
         self.dest_folder = pathlib.Path(dest_folder)
+        self.dest_folder_name = dest_folder_name
         self.repo = repo
         self.branch = branch
-        self.language = language
+        self.subfolder = subfolder
 
     @property
     def url(self):
@@ -58,35 +49,49 @@ class FileLocation:
 
     @property
     def move_folder(self):
-        return self.dest_folder / self.repo / self.language
+        return self.dest_folder / f"{self.repo}-{self.branch}" / self.subfolder
 
     @property
     def unzipped_folder(self):
-        return self.dest_folder / self.repo
+        return self.dest_folder / f"{self.repo}-{self.branch}"
 
     @property
     def os_tools_folder(self):
-        return self.dest_folder / "os_tools"
+        return self.dest_folder / self.dest_folder_name
 
 
-def copy_os_tools(dest_folder: str, branch="main", language="python"):
-    REPO = "tools-onderzoek-en-statistiek"
+def copy_repo(
+    repo: str,
+    dest_folder: str,
+    dest_folder_name,
+    branch,
+    subfolder,
+):
     fl = FileLocation(
-        dest_folder=dest_folder, repo=REPO, branch=branch, language=language
+        dest_folder=dest_folder,
+        dest_folder_name=dest_folder_name,
+        repo=repo,
+        branch=branch,
+        subfolder=subfolder,
     )
+    download(url=fl.url, dest_path=fl.zipfile)
+    unzip(zip_store=fl.zipfile, dest_folder=fl.dest_folder)
 
-    download(fl.url, dest_path=fl.zipfile)
-    unzip(fl.zipfile, fl.dest_folder)
-    move(fl.move_folder, fl.os_tools_folder)
+    if fl.os_tools_folder.exists():
+        shutil.rmtree(fl.os_tools_folder)
 
-    # zip_store.unlink()
+    shutil.move(fl.move_folder, fl.os_tools_folder)
+    fl.zipfile.unlink()
+    shutil.rmtree(fl.unzipped_folder)
 
-    # if fl.os_tools_folder.exists():
-    #     shutil.rmtree(fl.os_tools_folder)
 
-    # shutil.move(fl. , p_dest)
-    # shutil.rmtree(p_del)
+def copy_os_tools(dest_folder: str, branch="main", subfolder="python"):
+    REPO = "tools-onderzoek-en-statistiek"
+    DEST_FOLDER_NAME = "ostools"
+    if not branch:
+        branch = "main"
+    copy_repo(REPO, dest_folder, DEST_FOLDER_NAME, branch, subfolder)
 
 
 if __name__ == "__main__":
-    copy_os_tools(DEST_FOLDER)
+    copy_os_tools("C:/python_projects/_uitproberen/test_project/src")
