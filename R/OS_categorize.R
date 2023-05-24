bd066005233ccade4d524d3bd724b35ab846a248
JorisKlingen <jorisklingen@gmail.com>
2023-05-24 17:19:01 +0200

    add os_cut

--- R/OS_categorize.R
+++ R/OS_categorize.R
@@ -9,35 +9,50 @@ os_cut <- function(x,
                    start_label_overwrite = NULL,
                    end_label_overwrite = NULL){
   
-  n_labels <- length(breaks)
+  n_labels <- length(breaks) - 1
   
   start <- paste0(start_label, prefix, breaks[2], suffix)
   
-  end <- paste0(prefix, breaks[n_labels-1], suffix, end_label)
+  end <- paste0(prefix, breaks[n_labels], suffix, end_label)
   
   midden <- c()
   
-  for(i in 2:(n_labels - 3)) {
+  for(i in 2:(2 + n_labels - 2) ) {
+    
+    print(i)
     midden_add <- paste0(
       prefix, breaks[i], suffix, 
       sep,
       prefix, breaks[i + 1], suffix
     ) 
     
+    
     # append
     midden <- c(midden, midden_add)
     
   }
   
-  labels_manual <- c(start, midden, end)
+  if(n_labels > 2){
+    labels_manual <- c(start, midden, end)
+  }else{
+    labels_manual <- c(start, end)
+  }
+  
+  
+  cat('start: ', start, '\n', 
+      'midden: ', midden, '\n', 
+      'end: ', end, '\n')
+  cat('\n')
+  print(labels_manual)
   
   if(!is.null(end_label_overwrite)){
-    labels_manual[length(labels_manual)] <- end_label_overwrite
+    labels_manual[n_labels] <- end_label_overwrite
   }
   
   if(!is.null(start_label_overwrite)){
     labels_manual[1] <- start_label_overwrite
   }
+  
   
   cats <- cut(x, 
               breaks,
@@ -49,3 +64,6 @@ os_cut <- function(x,
   return(cats)
   
 } 
+
+
+
