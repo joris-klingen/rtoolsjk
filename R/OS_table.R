787ad0909e819a36a87840d16fa1e10037ef37ee
JorisKlingen <jorisklingen@gmail.com>
2023-05-31 10:56:53 +0200

    add features os_table

--- R/OS_table.R
+++ R/OS_table.R
@@ -32,7 +32,13 @@ get_table_styles <- function(){
 }
 
 
-ois_table <- function(df, path, sheet_name="Sheet1", add_perc_to_cols = NULL, left_align_cols = NULL){
+os_table <- function(df, 
+                     path, 
+                     sheet_name = "Sheet1", 
+                     perc_cols_index = NULL,
+                     perc_cols_pattern = NULL,
+                     left_align_char_cols = TRUE,
+                     left_align_index = NULL){
   
   wb <- createWorkbook()
   styles <- get_table_styles()
@@ -53,18 +59,40 @@ ois_table <- function(df, path, sheet_name="Sheet1", add_perc_to_cols = NULL, le
   addStyle_dflt_grd(style = styles$l_align, rows = 1, cols = cols)
   addStyle_dflt_grd(style = styles$r_align, rows = ex_top_r, cols = cols)
   
-  if(!is.null(add_perc_to_cols)){
+  
+  if(!is.null(perc_cols_pattern)){
+    left_cols_pat <- str_which(names(df), perc_cols_pattern) 
     addStyle_dflt_grd(style = styles$perc, rows = ex_top_r, cols = add_perc_to_cols)
+  }
+  
+  
+  if(!is.null(perc_cols_index)){
+    addStyle_dflt_grd(style = styles$perc, rows = ex_top_r, cols = add_perc_to_cols)
+  }
+  
+  
+  
+  # default left align char cols
+  if(left_align_char_cols){
+    char_cols <- unname(which(sapply(df, function(x) is.character(x)|is.factor(x) )))
+    addStyle_dflt_grd(style = styles$l_align, rows = ex_top_r, cols = left_align_cols)
   }
   
   if(!is.null(left_align_cols)){
     addStyle_dflt_grd(style = styles$l_align, rows = ex_top_r, cols = left_align_cols)
   }
   
+  
   # Set column width to 1.5 times the amount of chars
   setColWidths(wb, sheet_name, cols, widths = nchar(names(df)) + 4)
   saveWorkbook(wb, path, overwrite=TRUE)
 }
+
+
+
+
+
+
 
 
 style_sheet <- function(wb, df, sheet_name, add_perc_to_cols, left_align_cols){
@@ -89,6 +117,12 @@ style_sheet <- function(wb, df, sheet_name, add_perc_to_cols, left_align_cols){
     addStyle_dflt_grd(style = styles$perc, rows = ex_top_r, cols = add_perc_to_cols)
   }
   
+  
+  # left align char cols
+  char_cols <- unname(which(sapply(df, function(x) is.character(x)|is.factor(x) )))
+  addStyle_dflt_grd(style = styles$l_align, rows = ex_top_r, cols = left_align_cols)
+  
+  # overwrite left align based on index
   if(!is.null(left_align_cols)){
     addStyle_dflt_grd(style = styles$l_align, rows = ex_top_r, cols = left_align_cols)
   }
@@ -109,3 +143,8 @@ write_named_list_with_styling <- function(named_list, path){
   
   saveWorkbook(wb, path)
 }
+
+
+
+  
+
