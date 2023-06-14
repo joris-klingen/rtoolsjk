6ef5a54a867d685b0b7b9f85f1a93c5c4b3ba4cb
JorisKlingen <jorisklingen@gmail.com>
2023-06-14 16:54:10 +0200

    add colors

--- R/load_all.R
+++ R/load_all.R
@@ -16,7 +16,7 @@ source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main
 source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_vacature_tools.R', local = os_tools)
 source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_categorize.R', local = os_tools)
 
-# source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_colors.R', local = os_tools)
+source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_colors.R', local = os_tools)
 
 
 # activate
