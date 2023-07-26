d25cd4931feafdff377fbcc244b5295e80406e60
JorisKlingen <jorisklingen@gmail.com>
2023-07-26 16:03:17 +0200

    load all non overlap

--- R/load_all.R
+++ R/load_all.R
@@ -1,33 +1,33 @@
 library(rlang)
 
-# create os environment
-os_tools <- new.env(parent=globalenv())
 
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_db_configs.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_table.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_lookups.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_ggtheme.R', local = os_tools)
+if(!exists("os_tools")) {
 
-# oude versie
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OIS_ggtheme.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_statistiek_hulpfuncties.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_geolocate.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_get_geoms.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_vacature_tools.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_categorize.R', local = os_tools)
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_charts.R', local = os_tools)
+  # create os environment
+  os_tools <- new.env(parent=globalenv())
+  
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_db_configs.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_table.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_lookups.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_ggtheme.R', local = os_tools)
+  
+  # oude versie
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OIS_ggtheme.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_statistiek_hulpfuncties.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_geolocate.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_get_geoms.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_vacature_tools.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_categorize.R', local = os_tools)
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_charts.R', local = os_tools)
+  
+  source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_colors.R', local = os_tools)
 
-source('https://gitlab.com/os-amsterdam/tools-onderzoek-en-statistiek/-/raw/main/R/OS_colors.R', local = os_tools)
+  # activate
+  attach(os_tools)
 
+}
 
-# activate
-
-
-# if(!exists("os_tools")) {
-#   attach(os_tools)
-# }
-
-attach(os_tools)
+# attach(os_tools)
 
 os_show_functions <- function(env = os_tools) {
   print(names(env))
